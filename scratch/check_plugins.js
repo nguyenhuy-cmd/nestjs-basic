9a0e4b83b85670e62e0604a58776905ea41e3758
Nguyễn Huy <nguyenhuy140923@gmail.com>
2026-04-27 21:27:49 +0700

    chore: remove unused files and associated references

--- scratch/check_plugins.js
+++
@@ -1,7 +0,0 @@
-const softDeletePlugin = require('soft-delete-plugin-mongoose');
-const autopopulate = require('mongoose-autopopulate');
-
-console.log('softDeletePlugin type:', typeof softDeletePlugin);
-console.log('softDeletePlugin keys:', Object.keys(softDeletePlugin));
-console.log('autopopulate type:', typeof autopopulate);
-console.log('autopopulate keys:', Object.keys(autopopulate));
